1b1a20686280df51f64245acfab3a378bfa0cdba
Cândida Rosa Paraizo <43765601896@info.local>
2019-05-07 14:35:54 -0300

    Revisão Concluida

--- Banco de Dados/Script_Manipulação_Roman.sql
+++ Banco de Dados/Script_Manipulação_Roman.sql
@@ -4,3 +4,25 @@ USE DESAFIO_ROMAN;
 -----Sessão DELETE:-----
 DELETE FROM USUARIOS;
 DBCC CHECKIDENT('USUARIOS', RESEED, 0)
+
+
+GO
+
+CREATE PROCEDURE INSERIR_CLINICA
+AS
+BEGIN
+bulk insert CLINICA
+from 'C:\db\spmedgroup_clinica.csv'
+	with(
+		format = 'csv',
+		firstrow = 2, -- primeira linha de dados = 2 
+		fieldterminator = ';', -- separador de campos = ';'
+		rowterminator = '\n', -- separador de linhas = '\n'
+		CODEPAGE = 'ACP', -- codificação dos dados = 'ACP'
+		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
+	);
+END
+GO
+
+
+EXEC INSERIR_CLINICA
